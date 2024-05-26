0fda41cb949a13511c4323087b43a8ec6d6e0e5a
Shamil Kurbanov <kurbanov@outlook.de>
2024-05-26 22:26:47 +0200

    Refactor import statements and fix typos in 'reviews' app

--- bookr/reviews/management/commands/loadcsv.py
+++ bookr/reviews/management/commands/loadcsv.py
@@ -4,7 +4,8 @@ import re
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
 
-from bookr.reviews.models import Publisher, Contributor, Book, BookContributor, Review
+import reviews
+from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 
 
 class Command(BaseCommand):
@@ -46,7 +47,7 @@ class Command(BaseCommand):
             raise CommandError('File "{}" does not exist'.format(options['csv']))
 
         for data_dict in models.get('Publisher', []):
-            p, created = bookr.reviews.models.Publisher.objects.get_or_create(name=data_dict['publisher_name'], defaults={
+            p, created = reviews.models.Publisher.objects.get_or_create(name=data_dict['publisher_name'], defaults={
                 'website': data_dict['publisher_website'],
                 'email': data_dict['publisher_email']
             })
@@ -55,28 +56,28 @@ class Command(BaseCommand):
                 print('Created Publisher "{}"'.format(p.name))
 
         for data_dict in models.get('Book', []):
-            b, created = bookr.reviews.models.Book.objects.get_or_create(title=data_dict['book_title'], defaults={
+            b, created = reviews.models.Book.objects.get_or_create(title=data_dict['book_title'], defaults={
                 'publication_date': data_dict['book_publication_date'].replace('/', '-'),
                 'isbn': data_dict['book_isbn'],
-                'publisher': bookr.reviews.models.Publisher.objects.get(name=data_dict['book_publisher_name'])
+                'publisher': reviews.models.Publisher.objects.get(name=data_dict['book_publisher_name'])
             })
 
             if created:
                 print('Created Publisher "{}"'.format(b.title))
 
         for data_dict in models.get('Contributor', []):
-            c, created = bookr.reviews.models.Contributor.objects.get_or_create(first_names=data_dict['contributor_first_names'],
-                                                                                last_names=data_dict['contributor_last_names'],
-                                                                                email=data_dict['contributor_email'])
+            c, created = reviews.models.Contributor.objects.get_or_create(first_name=data_dict['contributor_first_name'],
+                                                                          last_name=data_dict['contributor_last_name'],
+                                                                          email=data_dict['contributor_email'])
 
             if created:
-                print('Created Contributor "{} {}"'.format(data_dict['contributor_first_names'],
-                                                           data_dict['contributor_last_names']))
+                print('Created Contributor "{} {}"'.format(data_dict['contributor_first_name'],
+                                                           data_dict['contributor_last_name']))
 
         for data_dict in models.get('BookContributor', []):
-            book = bookr.reviews.models.Book.objects.get(title=data_dict['book_contributor_book'])
-            contributor = bookr.reviews.models.Contributor.objects.get(email=data_dict['book_contributor_contributor'])
-            bc, created = bookr.reviews.models.BookContributor.objects.get_or_create(book=book,
+            book = reviews.models.Book.objects.get(title=data_dict['book_contributor_book'])
+            contributor = reviews.models.Contributor.objects.get(email=data_dict['book_contributor_contributor'])
+            bc, created = reviews.models.BookContributor.objects.get_or_create(book=book,
                                                                                      contributor=contributor,
                                                                                      role=data_dict['book_contributor_role'])
             if created:
@@ -88,9 +89,9 @@ class Command(BaseCommand):
 
             if created:
                 print('Created User "{}"'.format(creator.email))
-            book = bookr.reviews.models.Book.objects.get(title=data_dict['review_book'])
+            book = reviews.models.Book.objects.get(title=data_dict['review_book'])
 
-            review, created = bookr.reviews.models.Review.objects.get_or_create(content=data_dict['review_content'],
+            review, created = reviews.models.Review.objects.get_or_create(content=data_dict['review_content'],
                                                                                 book=book,
                                                                                 creator=creator,
                                                                                 defaults={
