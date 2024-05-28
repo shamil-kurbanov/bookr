efa3a4906cd44272edf85e4a45d0ef34b940a18c
Shamil Kurbanov <kurbanov@outlook.de>
2024-05-28 23:50:25 +0200

    Register models in admin site
    Register the models 'Publisher', 'Contributor', 'Book', 'BookContributor', and 'Review' in the Django admin site.

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from bookr.reviews.models import (Book, Publisher,
+                                  Contributor, BookContributor, Review)
+
+# Register models:
+admin.site.register(Publisher)
+admin.site.register(Contributor)
+admin.site.register(Book)
+admin.site.register(BookContributor)
+admin.site.register(Review)
